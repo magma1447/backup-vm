2435d606e33a95d6441814082f79e436c74a714f
root <root@1447.se>
2022-09-22 11:35:58 +0200

    Dump LV information for ignored disks as well

--- backup_vm/backup.py
+++ backup_vm/backup.py
@@ -71,7 +71,7 @@ def main():
         f.close()
 
         for disk in all_disks:
-            if disk in disks_to_backup and disk.type == "dev":
+            if disk.type == "dev":
                 lv = subprocess.check_output(["lvdisplay", disk.path], text=True)
                 f = open( (disk.path + ".lv")[1:].replace("/", "--"), "w" )
                 f.write(lv)
